395cf5c7f902dd685ed50e9e4923294c36b30b5c
Irina Boiko <irinaboiko95@gmail.com>
2021-09-13 17:35:17 +0300

    create server

--- client/src/components/Chat/components/ChatLayout/index.js
+++ client/src/components/Chat/components/ChatLayout/index.js
@@ -1,26 +1,115 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {
+  Avatar,
   Box,
   Button,
   Container,
   Grid,
   TextField,
+  Typography,
   withStyles,
 } from "@material-ui/core";
 
 import styles from "./styles";
 
+const interlocutor = true;
+
 const ChatLayout = ({ classes }) => {
   return (
     <Container className={classes.chatWrapper}>
       <Grid container className={classes.chatBlockWrapper}>
-        <Box className={classes.chatBlock}></Box>
+        <Grid
+          className={classes.chatBlock}
+          container
+          direction="column"
+          //justifyContent="flex-end"
+        >
+          <Grid
+            container
+            direction="column"
+            className={`${classes.messageWrapper} ${
+              interlocutor && classes.interlocutor
+            }`}
+          >
+            <Grid container alignItems="center" className={classes.userInfo}>
+              <Avatar>A</Avatar>
+              <Typography className={classes.userName}>Name</Typography>
+            </Grid>
+            Hi Hi Hi Hi Hi Hi
+          </Grid>
+          <Grid container direction="column" className={classes.messageWrapper}>
+            <Grid
+              container
+              alignItems="center"
+              className={`${classes.userInfo}`}
+            >
+              <Avatar>A</Avatar>
+              <Typography className={classes.userName}>Name</Typography>
+            </Grid>
+            Hi Hi Hi Hi Hi Hi
+          </Grid>
+          <Grid container direction="column" className={classes.messageWrapper}>
+            <Grid
+              container
+              alignItems="center"
+              className={`${classes.userInfo}`}
+            >
+              <Avatar>A</Avatar>
+              <Typography className={classes.userName}>Name</Typography>
+            </Grid>
+            Hi Hi Hi Hi Hi Hi
+          </Grid>
+          <Grid container direction="column" className={classes.messageWrapper}>
+            <Grid
+              container
+              alignItems="center"
+              className={`${classes.userInfo}`}
+            >
+              <Avatar>A</Avatar>
+              <Typography className={classes.userName}>Name</Typography>
+            </Grid>
+            Hi Hi Hi Hi Hi Hi
+          </Grid>
+          <Grid container direction="column" className={classes.messageWrapper}>
+            <Grid
+              container
+              alignItems="center"
+              className={`${classes.userInfo}`}
+            >
+              <Avatar>A</Avatar>
+              <Typography className={classes.userName}>Name</Typography>
+            </Grid>
+            Hi Hi Hi Hi Hi Hi
+          </Grid>
+          <Grid container direction="column" className={classes.messageWrapper}>
+            <Grid
+              container
+              alignItems="center"
+              className={`${classes.userInfo}`}
+            >
+              <Avatar>A</Avatar>
+              <Typography className={classes.userName}>Name</Typography>
+            </Grid>
+            Hi Hi Hi Hi Hi Hi
+          </Grid>
+          <Grid container direction="column" className={classes.messageWrapper}>
+            <Grid
+              container
+              alignItems="center"
+              className={`${classes.userInfo}`}
+            >
+              <Avatar>A</Avatar>
+              <Typography className={classes.userName}>Name</Typography>
+            </Grid>
+            Hi Hi Hi Hi Hi Hi
+          </Grid>
+        </Grid>
         <Grid
           container
           className={classes.messageBlock}
-          direction={"column"}
-          alignItems={"flex-end"}
+          direction="column"
+          alignItems="flex-end"
           //style={{width: '80%'}}
         >
           <TextField
--- client/src/components/Chat/components/ChatLayout/styles.js
+++ client/src/components/Chat/components/ChatLayout/styles.js
@@ -6,10 +6,36 @@ const styles = () => ({
   chatBlockWrapper: {
     height: "100%",
     flexDirection: "column",
+    flexWrap: "nowrap",
   },
   chatBlock: {
     backgroundColor: "#e0e0e0",
-    flex: "1 0 auto",
+    //flex: "1 0 auto",
+    height: "calc(100vh - 176px)",
+    flexWrap: "nowrap",
+    overflow: "hidden",
+    overflowY: "auto",
+  },
+  messageWrapper: {
+    fontSize: 14,
+    backgroundColor: " #83a3d4",
+    borderRadius: 5,
+    width: "fit-content",
+    maxWidth: "40%",
+    padding: 10,
+    margin: 10,
+    alignSelf: "flex-end",
+  },
+  interlocutor: {
+    backgroundColor: "#a9a9a9",
+    alignSelf: "flex-start",
+  },
+  userInfo: {
+    marginBottom: 5,
+  },
+  userName: {
+    fontSize: 12,
+    marginLeft: 5,
   },
   messageBlock: {
     flexShrink: 0,
